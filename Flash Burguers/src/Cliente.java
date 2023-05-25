aa968ceed6b4818404ef518100e1c25fdec235e7
thiaguin21 <101911121+thiaguin21@users.noreply.github.com>
2023-05-25 16:13:47 -0300

    Add files via upload

---
+++ Flash Burguers/src/Cliente.java
@@ -0,0 +1,13 @@
+public class Cliente extends CardapioDigital{
+
+    public Cliente(){
+        this.setPrecoTotal(0.0f);
+        this.setPrecoAcompanhamento(0.0f);
+        this.setPrecoHamburguer(0.0f);
+        this.setPrecoSobremesa(0.0f);
+        this.setPrecoBebida(0.0f);
+        this.setPrecoAdicionais(0.0f);
+        this.setAberto(false);
+    }
+
+}
---
+++ Flash Burguers/src/Pedido.java
@@ -0,0 +1,7 @@
+public interface Pedido {
+
+    public abstract void anotar();
+    public abstract void mostrar();
+
+
+}
